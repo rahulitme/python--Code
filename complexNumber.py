601edaa9f93cfb56a19b97ac603b3a5dc569d924
rahulitme <118755562+rahulitme@users.noreply.github.com>
2023-11-04 05:14:00 +1200

    Add files via upload

---
+++ complexNumber.py
@@ -0,0 +1,23 @@
+#Complex number inputs
+Var = complex(input('Enter an complex number value: '))
+print(Var)
+
+
+#mathematical operations on complex number
+#initialize complex number variables
+a = 2+4j
+b = 3+6j
+#addition
+print(a+b)
+
+
+#Subtraction
+print(a-b)
+
+
+#multiplication
+print(a*b)
+
+
+#division
+print(a/b)
