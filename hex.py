3cf3580618c2a336bf2c55997f41ec87f98fb5dd
rahulitme <118755562+rahulitme@users.noreply.github.com>
2023-11-24 04:19:26 +1200

    Add files via upload

---
+++ hex.py
@@ -0,0 +1,22 @@
+#integer number
+int_number = 14956
+#converting number to hexadecimal
+#using hex function
+Hexa_number = hex(int_number)
+print(str(Hexa_number) + ' is hexadecimal representation of number with base 10')
+
+
+#binary number
+bin_number = 0b101010
+#converting binary number to hexadecimal
+#using hex function
+Hexa_number = hex(bin_number)
+print(str(Hexa_number) + ' is hexadecimal representation of number with base 2')
+
+
+#octal number
+oct_number = 0o15437
+#converting octal number to hexadecimal
+#using hex function
+Hex_number = hex(oct_number)
+print(str(Hexa_number) + ' is hexadecimal representation of number with base 8')
