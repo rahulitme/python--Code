3430adb3c5ca7699cb52eb54d0445d73a16993df
rahulitme <118755562+rahulitme@users.noreply.github.com>
2023-11-26 04:03:46 +1200

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,34 @@
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    if y != 0:
+        return x / y
+    else:
+        return "Cannot divide by zero."
+
+# Get user input
+num1 = float(input("Enter first number: "))
+operator = input("Enter operator (+, -, *, /): ")
+num2 = float(input("Enter second number: "))
+
+# Perform calculation based on operator
+if operator == '+':
+    result = add(num1, num2)
+elif operator == '-':
+    result = subtract(num1, num2)
+elif operator == '*':
+    result = multiply(num1, num2)
+elif operator == '/':
+    result = divide(num1, num2)
+else:
+    result = "Invalid operator"
+
+# Display the result
+print(f"Result: {result}")
