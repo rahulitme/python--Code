6ae00e174a12cbaa8fbdd1ed41c21b7096175629
rahulitme <118755562+rahulitme@users.noreply.github.com>
2023-12-04 04:03:28 +1200

    Add files via upload

---
+++ hcf.py
@@ -0,0 +1,12 @@
+num1 = 36
+num2 = 60
+a = num1
+b = num2
+
+while num1 != num2:
+    if num1 > num2:
+        num1 -= num2
+    else:
+        num2 -= num1
+
+print("Hcf of", a, "and", b, "is", num1)
