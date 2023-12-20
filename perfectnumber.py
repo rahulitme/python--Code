ad2e69e69550093a0ae70798e4825484f667e24f
rahulitme <118755562+rahulitme@users.noreply.github.com>
2023-12-21 06:01:44 +1200

    Add files via upload

---
+++ perfectnumber.py
@@ -0,0 +1,15 @@
+from math import sqrt
+
+
+def isPerfectSquare(x):
+    if x >= 0:
+        sr = int(sqrt(x))
+        return (sr * sr) == x
+    return False
+
+
+n = 84
+if isPerfectSquare(n):
+    print("True")
+else:
+    print("False")
